8d2afffad0fdaaf633bcce7ab2a6d7c3025f129a
Shaun <shaun@thenetninja.co.uk>
2020-05-19 17:55:26 +0100

    lesson-17

---
+++ public/index.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', (e) => {
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+  } else {
+    menu.classList.add('hidden');
+  }
+});
